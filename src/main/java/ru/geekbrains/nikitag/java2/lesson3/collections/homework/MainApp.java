bc2d4443fce7c7e48971eb7e6a9cd014b75944d3
WinKeeper <winkeeper@mail.ru>
2021-02-22 14:35:02 +0500

    Initial commit

---
+++ src/main/java/ru/geekbrains/nikitag/java2/lesson3/collections/homework/MainApp.java
@@ -0,0 +1,66 @@
+package ru.geekbrains.nikitag.java2.lesson3.collections.homework;
+
+import java.util.*;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(Arrays.asList(
+                "Cat", "Cat", "Dog", "Better",
+                "Better", "Day", "Cat", "Better",
+                "Then", "Cat", "Day", "Better",
+                "Day", "Better", "Cat", "Dog",
+                "Can", "Do", "While", "Cat", "Better"));
+
+        //Убираем повторения
+        removeClones(list);
+        //Считаем значения
+        countList(list);
+
+
+
+        System.out.println("");
+        //Задание №2 Телефонный справочник
+        System.out.println("Задание 2 - Телефонный справочник");
+
+        TelephoneBook book1 = new TelephoneBook(new HashMap<>());
+        book1.addPerson("Kirill", new ArrayList<>(Arrays.asList("51-52-53", "54-56-67")));
+        book1.addPerson("Kirill", new ArrayList<>(Arrays.asList("51-3-53", "54-564-67")));
+        book1.addPerson("Vasia", new ArrayList<>(Arrays.asList("54-522-53", "414-99-67")));
+        book1.addPerson("Fokiar", new ArrayList<>(Arrays.asList("54-838-67")));
+        book1.addPerson("Fokiar", new ArrayList<>(Arrays.asList("41-5112-53")));
+        book1.addPerson("Gavriil", new ArrayList<>(Arrays.asList("56-582-41")));
+        book1.getPerson("Vasia");
+
+    }
+
+    //Мне кажется интересный вариант через Set убрать двойников :) + можно прикрутить compare
+    public static Set<String> removeClones(List<String> list) {
+        Set<String> tempSet = new HashSet<>();
+        for (int i = 0; i < list.size(); i++) {
+            tempSet.add(list.get(i));
+        }
+        System.out.println(tempSet);
+        return tempSet;
+    }
+
+    //Подсчёт через создание второй таблицы и удаления её элементов по мере наращивания счётчика
+    public static void countList(List<String> list) {
+        List<String> list2 = list;
+        do {
+            Iterator<String> iter = list2.iterator();
+            int counter = 0;
+            String tempName = list2.get(0);
+            while (iter.hasNext()) {
+                String str = iter.next();
+                if (str.equals(tempName)) {
+                    counter++;
+                    iter.remove();
+                }
+            }
+            System.out.println(tempName + " встречается в списке " + counter + " раз.");
+        } while (list2.size() != 0);
+    }
+
+
+}
---
+++ src/main/java/ru/geekbrains/nikitag/java2/lesson3/collections/homework/TelephoneBook.java
@@ -0,0 +1,28 @@
+package ru.geekbrains.nikitag.java2.lesson3.collections.homework;
+
+import java.util.*;
+
+public class TelephoneBook {
+    Map<String, List<String>> book;
+
+    public TelephoneBook(Map<String, List<String>> book) {
+        this.book = book;
+    }
+
+    public void addPerson(String name, ArrayList<String> telephone) {
+        book.put(name, telephone);
+    }
+
+    public Map<String, List<String>> getBook() {
+        return book;
+    }
+
+    //Не могу разобраться, как нам работать с типом value для Map List<String>, постоянно ругается на неправильно предоставленный тип
+    public void getPerson(String name) {
+        Iterator iter = book.entrySet().iterator();
+        while (iter.hasNext()) {
+            System.out.println(name + ". Телефон: " + book.get(name));
+            iter.next();
+        }
+    }
+}
